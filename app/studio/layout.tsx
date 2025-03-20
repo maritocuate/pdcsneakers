e777f34457f6e00ef84faae1ea074dc00b98fb8f
maritocuate <mario300@gmail.com>
2025-03-20 09:07:09 -0500

    ✨ init app and sanity+clerk

---
+++ app/studio/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "PDC Sneakers",
+  description: "Sneakers Store",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
