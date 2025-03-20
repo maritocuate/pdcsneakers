2556f12a3edcd87009f2653aaded8551bb567f92
maritocuate <mario300@gmail.com>
2025-03-20 10:44:26 -0500

    ✨ header component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,54 @@
+"use client";
+
+import { ClerkLoaded, SignInButton, useUser, UserButton } from "@clerk/nextjs";
+import Link from "next/link";
+import Form from "next/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "./ui/button";
+import { PackageIcon, TrolleyIcon } from "@sanity/icons";
+
 function Header() {
-  return <header>Header</header>;
+  const { user } = useUser();
+  console.log(user);
+
+  return (
+    <header className="flex flex-wrap justify-between items-center gap-4 p-4 py-2">
+      <Link href="/">PDC Sneakers</Link>
+
+      <Form action="/search" className="w-full sm:flex-1">
+        <Input
+          type="text"
+          name="query"
+          placeholder="Search"
+          className="rounded"
+        />
+      </Form>
+
+      <div className="flex items-center gap-4">
+        <Link href="/basket">
+          <Button>
+            <TrolleyIcon className="w-4 h-4" />
+          </Button>
+        </Link>
+
+        <ClerkLoaded>
+          {user ? (
+            <div className="flex items-center gap-3">
+              <Link href="/orders">
+                <Button>
+                  <PackageIcon className="w-4 h-4" />
+                </Button>
+              </Link>
+
+              <UserButton />
+            </div>
+          ) : (
+            <SignInButton mode="modal" />
+          )}
+        </ClerkLoaded>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
