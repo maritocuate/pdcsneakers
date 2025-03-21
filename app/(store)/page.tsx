069968af177b1cc1086c6f80125aae476e809992
maritocuate <mario300@gmail.com>
2025-03-21 14:30:26 -0500

    ✨ homepage update

--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@/components/ui/button";
 import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
 import ProductsView from "@/components/ProductsView";
 export default async function Home() {
@@ -6,8 +5,6 @@ export default async function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      PDC Sneakers Home
-      <Button>Click me</Button>
       <ProductsView products={products} />
     </div>
   );
