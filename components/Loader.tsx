b3cdd041c163983327ae2ae0ad31568c933f1d2a
maritocuate <mario300@gmail.com>
2025-03-24 14:28:29 -0500

    ✨ loader component

---
+++ components/Loader.tsx
@@ -0,0 +1,9 @@
+function Loader() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-10 h-10 border-t-2 border-b-2 border-gray-900 rounded-full animate-spin"></div>
+    </div>
+  );
+}
+
+export default Loader;
