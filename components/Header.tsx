2f57a009a91580603824d688f6b58f8407914048
maritocuate <mario300@gmail.com>
2025-03-26 13:01:54 -0500

    ✨ header update

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,46 +5,50 @@ import Link from "next/link";
 import Form from "next/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "./ui/button";
-import { PackageIcon, TrolleyIcon } from "@sanity/icons";
-
+import Image from "next/image";
+import { Package, ShoppingCart } from "lucide-react";
 function Header() {
   const { user } = useUser();
 
   return (
-    <header className="flex flex-wrap justify-between items-center gap-4 p-4 py-2">
-      <Link href="/">PDC Sneakers</Link>
+    <header className="flex flex-wrap justify-between items-center gap-4 p-6">
+      <Link href="/">
+        <Image src="/logo.png" alt="PDC Sneakers" width={80} height={80} />
+      </Link>
 
-      <Form action="/search" className="w-full sm:flex-1">
-        <Input
-          type="text"
-          name="query"
-          placeholder="Search"
-          className="rounded"
-        />
-      </Form>
+      <div className="flex flex-wrap justify-between items-center gap-4">
+        <Form action="/search" className="w-full sm:flex-1">
+          <Input
+            type="text"
+            name="query"
+            placeholder="Search"
+            className="rounded-full bg-slate-100"
+          />
+        </Form>
 
-      <div className="flex items-center gap-4">
-        <Link href="/basket">
-          <Button>
-            <TrolleyIcon className="w-4 h-4" />
-          </Button>
-        </Link>
+        <div className="flex items-center gap-2">
+          <Link href="/basket">
+            <Button variant="outline" className="rounded-full w-9 h-9">
+              <ShoppingCart size={20} />
+            </Button>
+          </Link>
 
-        <ClerkLoaded>
-          {user ? (
-            <div className="flex items-center gap-3">
-              <Link href="/orders">
-                <Button>
-                  <PackageIcon className="w-4 h-4" />
-                </Button>
-              </Link>
+          <ClerkLoaded>
+            {user ? (
+              <div className="flex items-center gap-2">
+                <Link href="/orders">
+                  <Button variant="outline" className="rounded-full w-9 h-9">
+                    <Package />
+                  </Button>
+                </Link>
 
-              <UserButton />
-            </div>
-          ) : (
-            <SignInButton mode="modal" />
-          )}
-        </ClerkLoaded>
+                <UserButton />
+              </div>
+            ) : (
+              <SignInButton mode="modal" />
+            )}
+          </ClerkLoaded>
+        </div>
       </div>
     </header>
   );
